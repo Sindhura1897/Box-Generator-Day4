d2da043c43f3c3e6b2d6d068cd96ff6cb204f86a
Sindhura1897 <65909663+Sindhura1897@users.noreply.github.com>
2020-07-31 17:27:37 +0530

    Update generator.js

--- generator.js
+++ generator.js
@@ -2,11 +2,13 @@ $number1=document.getElementById("text1").value;
 var count=0;
 function generator() {
     $number1=document.getElementById("text1").value;
+    $color1=document.getElementById("color1").value;
     for(var i=0;i<$number1;i++){
         count++;
         btn = document.createElement("button");
         btn.id="btn1";
         btn.innerHTML="New Button " +count;
+        btn.style="background-color:" +($color1) + ";"
         document.getElementById("new").appendChild(btn);
     }
 }
