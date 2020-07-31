ced446d169788ce3175ba49ea0f1b25f66fea72d
Sindhura1897 <65909663+Sindhura1897@users.noreply.github.com>
2020-07-31 12:58:12 +0530

    JS

--- generator.js
+++ generator.js
@@ -1,22 +1,7 @@
-/*$number1=document.getElementById("text");
-var count=0;
-
-function generator(){
-    for(var i=0; i<=$number1; i++){
-        count++;
-        btn=document.createElement("button");
-        btn.id="btn1";
-        document.getElementById("container").appendChild("btn1");
-        
-        btn.style.display="inherit";
-        btn.innerHTML="New Button"+count;
-    }
-}*/
 $number1=document.getElementById("text1").value;
 var count=0;
 function generator() {
     $number1=document.getElementById("text1").value;
-    $color1=document.getElementById("color1").value;
     for(var i=0;i<$number1;i++){
         count++;
         btn = document.createElement("button");
